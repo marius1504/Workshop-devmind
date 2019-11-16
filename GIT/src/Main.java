ab1b9f61ad06bf14885837a1bac80af036543800
marius1504 <nichiteleamarius@gmail.com>
2019-11-16 12:27:54 +0200

    modificari

--- GIT/src/Main.java
+++ GIT/src/Main.java
@@ -3,10 +3,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("fsdfsdfsdf");
-		System.out.println("Add");
+		System.out.println("1+@3213+#21");
 		System.out.println("iona are mere");
-		System.out.println("sadsadsadasdas");
+		System.out.println("");
 		System.out.println("am inteles");
+		System.out.println("dhasghajasghfa");
 	}
 
 }

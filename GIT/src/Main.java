bc6afc6c895f20d3c9c69be718a656fa09fa6612
marius1504 <nichiteleamarius@gmail.com>
2019-11-16 12:09:59 +0200

    test

--- GIT/src/Main.java
+++ GIT/src/Main.java
@@ -4,6 +4,8 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("fsdfsdfsdf");
 		System.out.println("Add");
+		System.out.println("iona are mere");
+		System.out.println("sadsadsadasdas");
 	}
 
 }
